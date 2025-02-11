b79f258b221f5e90f99d3d306881a92cc07e8fdc
910jmiguel <910jmiguel@gmail.com>
2025-02-10 21:51:13 -0500

    lab1 and lab2 added

---
+++ PracticeLab/ICPQ0.java
@@ -0,0 +1,40 @@
+public class ICPQ0 {
+	
+	private ICPQ0() {
+		// empty to prevent object creation
+		// ALREADY IMPLEMENTED; DO NOT MODIFY
+	}
+	
+
+	/**
+	 * The parameters a and b are two integers. 
+	 * Return true if either one is 10 but not both, or if their sum is 10.
+	 * Example calls:
+	 * makes10(9, 10) return true
+	 * makes10(9, 9) return false
+	 * makes10(1, 9) return true
+	 * makes10(10, 0) return true
+	 * makes10(10, 10) return false
+	 * 
+	 * @param a is integer input
+	 * @param b is integer input
+	 * @return true if either one is 10 or if their sum is 10
+	 */
+	public static boolean makes10(int a, int b) {
+		/* Your implementation of this method starts here. 
+		 * Recall that :
+		 * 1. No System.out.println statements should appear here.
+		 * 	  Instead, you need to return the result.
+		 * 2. No Scanner operations should appear here (e.g., input.nextInt()).
+		 *    Instead, refer to the input parameters of this method.   
+		 */
+		
+
+        return true;
+	}
+
+    public static void main(String[] args) {
+        System.out.println(makes10(9, 10));
+    }
+
+}
